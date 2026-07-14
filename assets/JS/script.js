6be3ad8d28e0f3531973b2150fdf00c3018ebfa7
Joab Cabreira <joabfla15@gmail.com>
2026-07-13 21:45:46 -0400

    modified script.js - add more options in function

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -15,16 +15,71 @@ const select_toCurrency = toCurrency.value
 
 // Função para conversão de moedas
 async function convert() {
-
-    // Importa os dados de conversao em tempo real
-    const resposta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
+    const resposta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL,USD-EUR,USD-CLP,BRL-USD,BRL-EUR,BRL-CLP,EUR-USD,EUR-CLP,EUR-BRL,CLP-USD,CLP-EUR,CLP-BRL");
     const dados = await resposta.json();
 
-    // Verifica quais os selects e define qual sera a cotação
+    // Dolar para Real BR
     if (fromCurrency.value === "usd" && toCurrency.value === "brl"){
         var calculo = entered_value.value * dados.USDBRL.bid;
-        
-        // Input mostrando os resultados
         document.getElementById("resultado").value = calculo.toFixed(2);
+    }
+    // Dolar USD para Euro Eur
+    else if (fromCurrency.value === "usd" && toCurrency.value === "eur"){
+        var calculo = entered_value.value * dados.USDEUR.bid;
+        document.getElementById("resultado").value = calculo.toFixed(2);
+    }
+     // Dolar USD para Peso chileno CLP
+    else if (fromCurrency.value === "usd" && toCurrency.value === "clp"){
+        var calculo = entered_value.value * dados.USDCLP.bid;
+        document.getElementById("resultado").value = calculo.toFixed(2);
+    }
+    // Real Br para Dolar USD
+    else if (fromCurrency.value === "brl" && toCurrency.value === "usd"){
+        var calculo = entered_value.value * dados.BRLUSD.bid;
+        document.getElementById("resultado").value = calculo.toFixed(2);
+    }
+    // Real Br para Euro Eur
+    else if (fromCurrency.value === "brl" && toCurrency.value === "eur"){
+        var calculo = entered_value.value * dados.BRLEUR.bid;
+        document.getElementById("resultado").value = calculo.toFixed(2);
+    }
+    // Real para Peso chileno CLP
+    else if (fromCurrency.value === "brl" && toCurrency.value === "clp"){
+        var calculo = entered_value.value * dados.BRLCLP.bid;
+        document.getElementById("resultado").value = calculo.toFixed(2);
+    }
+    // Euro Eur para dolar usd
+    else if (fromCurrency.value === "eur" && toCurrency.value === "usd"){
+        var calculo = entered_value.value * dados.EURUSD.bid;
+        document.getElementById("resultado").value = calculo.toFixed(2);
+    }
+    // Euro Eur para Peso chileno CLP
+    else if (fromCurrency.value === "eur" && toCurrency.value === "clp"){
+        var calculo = entered_value.value * dados.EURCLP.bid;
+        document.getElementById("resultado").value = calculo.toFixed(2);
+    }
+    // Euro Eur para Real Brasileiro
+    else if (fromCurrency.value === "eur" && toCurrency.value === "brl"){
+        var calculo = entered_value.value * dados.EURBRL.bid;
+        document.getElementById("resultado").value = calculo.toFixed(2);
+    }
+    // Peso chileno para Real BR
+    else if (fromCurrency.value === "clp" && toCurrency.value === "brl"){
+        var calculo = entered_value.value * dados.CLPBRL.bid;
+        document.getElementById("resultado").value = calculo.toFixed(2);
+    }
+    // Peso chileno para Euro eur
+    else if (fromCurrency.value === "clp" && toCurrency.value === "eur"){
+        var calculo = entered_value.value * dados.CLPEUR.bid;
+        document.getElementById("resultado").value = calculo.toFixed(2);
+    }
+    // Peso chileno para dolar usd
+    else if (fromCurrency.value === "clp" && toCurrency.value === "usd"){
+        var calculo = entered_value.value * dados.CLPUSD.bid;
+        document.getElementById("resultado").value = calculo.toFixed(2);
+    }
+    else{
+        mesmo_valor = entered_value.value;
+        return document.getElementById("resultado").value = mesmo_valor;
     }
 }
